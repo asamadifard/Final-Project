23812261a152c20c887bad717a6cccde4c439eb3
Adam Samadifard <asamadifard@ivytech.edu>
2025-12-18 10:52:10 -0500

    db fix - aas

--- school.js
+++ school.js
@@ -87,7 +87,7 @@ try{
     res.status(401).json({error: "invalid jwt"})
 }
 
-router.post("/auth", async(req,res){
+router.post("/auth", async(req,res)=>{
     if(!req.body.username || !req.body.password){
         res.status(400).json({error: "Missing username or password"})
         return
